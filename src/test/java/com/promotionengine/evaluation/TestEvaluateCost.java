d9b0b7a6a3fec3144ff06393026176770a085e0f
veereshkaladagi <87117526+veereshkaladagi@users.noreply.github.com>
2022-04-26 15:45:14 +0530

    Update TestEvaluateCost.java
    3 sceanrio test cases

--- src/test/java/com/promotionengine/evaluation/TestEvaluateCost.java
+++ src/test/java/com/promotionengine/evaluation/TestEvaluateCost.java
@@ -1,10 +1,30 @@
-package com.promotionengine.evaluate;
+package com.promotionengine.calculation;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
-public class TestEvaluateCost {
+public class TestCalculcateCost {
 	
-
+	private CalculateCost testee = new CalculateCost();
+	
+	//Scenario A
+	@Test
+	public void testCalculation1() {
+		int result = testee.calculateTotalCost(1,1,1,0);
+		assertThat(result).isEqualTo(100);
+	}
+	//Scenario B
+	@Test
+	public void testCalculation2() {
+		int result = testee.calculateTotalCost(5,5,1,0);
+		assertThat(result).isEqualTo(370);
+	}
+	
+	//Scenario C
+	@Test
+	public void testCalculation3() {
+		int result = testee.calculateTotalCost(3,5,1,1);
+		assertThat(result).isEqualTo(280);
+	}
 }
